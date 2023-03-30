83a31c6215095ca86c3d786f6b93f7b1b35ec5cc
davidryan316 <97624828+davidryan316@users.noreply.github.com>
2023-03-30 16:57:47 +0100

    Update game.test.js
    new path

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 describe('App', () => {
   it('Contains the compiled JavaScript', (done) => {
-    fs.readFile('./__test__/game.test.js', 'utf8', (err, data) => {
+    fs.readFile('New-Node.js-Workflow/__test__/game.test.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
       done()
