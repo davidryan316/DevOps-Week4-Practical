1ee18b9b8481b5f8825bc76941bae240af9aef07
davidryan316 <97624828+davidryan316@users.noreply.github.com>
2023-03-30 16:36:16 +0100

    Update game.test.js
    ok

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 describe('App', () => {
   it('Contains the compiled JavaScript', (done) => {
-    fs.readFile('https://github.com/davidryan316/DevOps-Week4-Practical/blob/NewBranch-Node.JS-Workflow/__test__/game.test.js', 'utf8', (err, data) => {
+    fs.readFile('.public/__test__/game.test.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
       done()
